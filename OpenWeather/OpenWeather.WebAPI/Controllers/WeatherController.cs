0fdb747b108b09168bf347b6d3c29375563ef0b0
Denis Diedov <80519500+Wan-de-Rock@users.noreply.github.com>
2023-05-10 17:06:27 +0200

    Project rename

---
+++ OpenWeather/OpenWeather.WebAPI/Controllers/WeatherController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenWeatherWebAPI.DTOs;
+using OpenWeatherWebAPI.Exceptions;
+using OpenWeatherWebAPI.Services;
+
+namespace OpenWeatherWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet("{city}/{country}")]
+        public async Task<ActionResult<Weather>> GetCityWeather(string city, string country)
+        {
+            Weather weather = null!;
+
+            try { weather = await WeatherDataProvider.GetCityWeather(city, country); }
+            catch (ArgumentNullException argNullEx) { return BadRequest(argNullEx.Message); }
+            catch (NotFoundException notFoundEx) { return NotFound(notFoundEx.Message); }
+            catch (HttpRequestException httpEx) { 
+                var objectResult = new ObjectResult(httpEx.StatusCode);
+                //objectResult.Value = weather;
+                return objectResult;
+            }
+            catch (Exception) { return StatusCode(500); }
+
+            return Ok(weather);
+        }
+    }
+}

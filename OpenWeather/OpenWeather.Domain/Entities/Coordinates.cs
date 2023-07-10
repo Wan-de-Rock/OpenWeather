08e9734ccd5649c83332b68c4d075fd22d9c0781
Denis Diedov <80519500+Wan-de-Rock@users.noreply.github.com>
2023-07-10 23:39:28 +0200

    Delete unnecessary files

--- OpenWeather/OpenWeather.Domain/Entities/Coordinates.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OpenWeather.Domain.Entities;
-
-public struct Coordinates
-{
-    public double Latitude { get; set; }
-
-    public double Longitude { get; set; }
-}
--- OpenWeather/OpenWeather.Domain/Interfaces/IGeocodingRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OpenWeather.Domain.Interfaces;
-
-using OpenWeather.Domain.Entities;
-
-public interface IGeocodingRepository
-{
-    public Task<CityData> GetCityData(string city, string country);
-    public IReadOnlyCollection<string> GetCountryNames();
-}

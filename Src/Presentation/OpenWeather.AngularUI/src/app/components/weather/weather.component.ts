5845bf5c7b9487f943eb02d10bd702d784797ab4
Denis Diedov <80519500+Wan-de-Rock@users.noreply.github.com>
2023-08-31 11:55:43 +0200

    Add geocoding service & dropdown field for countries

--- Src/Presentation/OpenWeather.AngularUI/src/app/components/weather/weather.component.ts
+++ Src/Presentation/OpenWeather.AngularUI/src/app/components/weather/weather.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Weather } from 'src/app/models/weather.model';
+import { GeocodingService } from 'src/app/services/geocoding.service';
 import { WeatherService } from 'src/app/services/weather.service';
 
 @Component({
@@ -17,12 +18,16 @@ export class WeatherComponent implements OnInit {
 
     cityName: string = "";
     countryName: string = "";
+    countriesNames: string[] = [];
+
     @Input()
     weather = new Weather();
 
-    constructor(private weatherService: WeatherService) { }
+    constructor(private weatherService: WeatherService,
+                private geocodingService: GeocodingService) { }
 
     ngOnInit() {
+        this.getCountriesNames();
         this.getWeather(this.DEFAULT_CITY_NAME, this.DEFAULT_COUNTRY_NAME);
     }
 
@@ -30,9 +35,18 @@ export class WeatherComponent implements OnInit {
         this.getWeather(this.cityName, this.countryName);
     }
 
+    changeCountry(event: any){
+        this.countryName = event.target.value;
+    }
+
     private getWeather(city: string, country: string){
         //this.weather = await this.weatherService.getWeather(city, country);
         this.weatherService.getWeatherFromCustomApi(city, country)
         .subscribe((response: Weather) => (this.weather = response));
+    }
+
+    private getCountriesNames(){
+        this.geocodingService.getCountriesNamesSorted()
+        .subscribe((response: string[]) => (this.countriesNames = response));
     }
 }
---
+++ Src/Presentation/OpenWeather.AngularUI/src/app/services/geocoding.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class GeocodingService {
+    private api_url = "https://localhost:7002";
+
+    constructor(private http: HttpClient) { }
+
+    getCountriesNamesSorted(): Observable<string[]> {
+        let url_weather = `${this.api_url}/api/Geocoding`;
+
+        return this.http.get<string[]>(url_weather);
+    }
+}
